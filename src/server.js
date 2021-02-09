00830f71c97a5c2707a457c49c942468db1286ed
Isaac Hughes <isaachughesbusiness@gmail.com>
2021-02-09 14:05:06 +0000

    FS10 branch added

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 require('./db/connection')
 const express = require('express')
 const cors = require('cors')
+const { Record } = require("./models/Records")
+const { recordRouter } = require("./routes/records")
 
 const port = process.env.PORT || 5000
 const app = express()
 
+app.use(cors())
 app.use(express.json())
+app.use(recordRouter)
 
 app.get("/health", (req, res) => {
     res.status(200).send({ message: "Backend Working"})
