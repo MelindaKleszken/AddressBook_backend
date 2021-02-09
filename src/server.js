fee40ac4a43fa555cb94792f397d9c11e64d5acb
Isaac Hughes <isaachughesbusiness@gmail.com>
2021-02-09 14:29:58 +0000

    Added testing

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const { Record } = require("./models/Records")
 const { recordRouter } = require("./routes/records")
+const { userRouter } = require("./routes/user")
 
 const port = process.env.PORT || 5000
 const app = express()
@@ -10,6 +11,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(recordRouter)
+app.use(userRouter)
 
 app.get("/health", (req, res) => {
     res.status(200).send({ message: "Backend Working"})
